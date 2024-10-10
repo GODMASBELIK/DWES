28c0af35792a8d0cf2a0384bffff92683f14bce3
GODMASBELIK <164207686+GODMASBELIK@users.noreply.github.com>
2024-10-10 15:18:18 +0200

    Delete A lo guarro directory

--- A lo guarro/woo.js
+++
@@ -1,7 +0,0 @@
-const elem = document.getElementById("YOLO");
-
-
-function ASD() {
-    console.log();  // Muestra el botón que ha disparado el evento
-    elem.style.color = "red";   // Cambia el color del elemento YOLO
-    }
